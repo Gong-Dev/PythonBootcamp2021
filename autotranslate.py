450445c6aa5a6fc39dffe9f6318d3e60cad6252c
Gong-Dev <78164101+Gong-Dev@users.noreply.github.com>
2021-02-09 00:07:48 +0700

    Add files via upload

--- autotranslate.py
+++ autotranslate.py
@@ -1,9 +1,12 @@
 #autotranslate.py
 #pip install googletrans==3.1.0a0
+#pip install easyread
+#pip install openpyxl
 
-from googletrans import Translator
-
-trans=Translator()
+# from googletrans import Translator
+from easyread.translator import Translate
+from openpyxl import Workbook
+from datetime import datetime
 # result=trans.translate('cat',dest='th')
 # print(result.text)
 
@@ -13,9 +16,24 @@ article=article.split()
 
 print ('count',len(article))
 
-result={}
+result=[]
 
 for word in article:
 	# print(word)
-	res=trans.translate(word,dest='th')
-	print(res.text)
+	res=Translate(word)
+	if res != None:  #บางครั้ง
+		# print(res['meaning'])
+		result.append([word,res['meaning']])  #result.append['cat','แมว']
+# print(result)
+
+excelfile=Workbook()
+sheet=excelfile.active
+
+header=['Vocab','Translate']
+sheet.append(header)
+
+for r in result:
+	sheet.append(r)
+
+dt=datetime.now().strftime('%Y-%m-%d %H%M%S')
+excelfile.save('Vocab-{}.xlsx'.format(dt))
